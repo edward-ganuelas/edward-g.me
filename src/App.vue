a4a989ec2d3896abbae2347ad800fc567e4b6ecb
Edward Ganuelas <epganuelas@gmail.com>
2018-01-08 11:54:53 -0500

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <v-app>
+      <v-content>
+      <router-view/>
+      </v-content>
+    </v-app>
   </div>
 </template>
 
@@ -11,7 +15,7 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+require('../node_modules/vuetify/dist/vuetify.min.css');
+
+Vue.use(Vuetify)
+
 
 Vue.config.productionTip = false
 
