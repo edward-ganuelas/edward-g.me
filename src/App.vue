666f5213fe2888da7fea74609a13f28986066cec
Edward Ganuelas <epganuelas@gmail.com>
2018-01-08 12:03:49 -0500

    Added Home component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <v-app>
       <v-content>
       <router-view/>
---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'W'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
