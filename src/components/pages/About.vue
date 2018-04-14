0caba560968077ead70f50da6059b919a1154e68
Edward Ganuelas <epganuelas@gmail.com>
2018-04-13 20:34:59 -0400

    Add animate.css, add about me page, add about me route, add icon

---
+++ src/components/pages/About.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "About",
+  data() {
+    return {
+      content: "",
+      meta: {
+          title: "About Me",
+          description: "Personal Site of Edward Ganuelas",
+          keywords: "developer, javascript, photography, filipino, blog, nikon, gaming, basketball, raptors, nba, wrestling, wwe"
+      }
+    };
+  },
+
+  methods: {
+    getContent: function() {
+      axios.get("static/json/main.json").then(x => {
+        this.content = x.data.content;
+      });
+    }
+  },
+  beforeMount: function() {
+    // this.getContent();
+  },
+  head: {
+    title: function() {
+      return {
+        inner: this.meta.title
+      };
+    },
+    meta: function() {
+      return [
+        { name: "description", content: this.meta.description },
+        { name: "keywords", content: this.meta.keywords }
+      ];
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.main-content{
+  @media(min-width: 1024px){
+    padding-top: 120px;
+  }
+}
+</style>
