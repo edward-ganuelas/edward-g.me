7993f4ef0c3319097d10cbf01e83cb50dbf6b187
Edward Ganuelas <epganuelas@gmail.com>
2018-07-24 21:39:32 -0400

    Photography page updates

--- src/components/pages/Photography.vue
+++ src/components/pages/Photography.vue
@@ -1,13 +1,72 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-xs-12 col-sm-8">
+      <div class="col-12 col-sm-8">
         <h2>Photography</h2>
       </div>
     </div>
     <div class="row">
-      <div class="col-xs-12 col-sm-8">
+      <div class="col-12 col-sm-8">
         <h3>A section about a hobby</h3>
+      </div>
+    </div>
+
+    <div class="row gear">
+      <div class="container">
+        <div class="row">
+           <div class="col-12 col-sm-8">
+            <h3>Gear</h3>
+           </div>
+        </div>
+        <div class="row">
+          <div class="col-12 col-sm">
+            <h4>Cameras</h4>
+            <ul>
+              <li>Nikon EM (2)</li>
+              <li>Nikon FM</li>
+              <li>Nikon F4</li>
+              <li>Nikon D7000</li>
+              <li>Nikon D700</li>
+              <li>Fujica GW690</li>
+              <li>Yashica Electro</li>
+            </ul>
+          </div>
+          <div class="col-12 col-sm">
+            <h4>Lenses</h4>
+            <ul>
+              <li>Nikkor 28mm f2.8 AI-s</li>
+              <li>Nikkor 35mm f2 AF-D</li>
+              <li>Nikkor 35mm f1.8G AF-S DX</li>
+              <li>Nikon Series E 50mm f1.8</li>
+              <li>Micro-Nikkor 55mm f2.8 AI-s</li>
+              <li>Helios 44-2</li>
+              <li>Nikon Series E 100mm f2.8</li>
+              <li>Nikon Series E 70-210mm f4</li>
+            </ul>
+          </div>
+          <div class="col-12 col-sm">
+            <h4>Misc</h4>
+            <ul>
+              <li>Nikon SB-24</li>
+            
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row photos">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-8">
+            <h3>Photos</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 col-sm">
+            <p>Still working on this section. Come back soon! In the meanwhile, check out my <a href="https://www.instagram.com/mustadio98/" target="_blank" rel="noopener noreferrer">instagram</a>.</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -21,16 +80,15 @@ export default {
     return {
       content: "",
       meta: {
-          title: "Photography",
-          description: "Personal Site of Edward Ganuelas",
-          keywords: "developer, javascript, photography, filipino, blog, nikon, gaming, basketball, raptors, nba, wrestling, wwe"
+        title: "Photography",
+        description: "Personal Site of Edward Ganuelas",
+        keywords:
+          "developer, javascript, photography, filipino, blog, nikon, gaming, basketball, raptors, nba, wrestling, wwe"
       }
     };
   },
 
-  methods: {
-  
-  },
+  methods: {},
   beforeMount: function() {
     // this.getContent();
   },
@@ -52,5 +110,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+.gear, .photos {
+  margin-top: 50px;
+}
 </style>
