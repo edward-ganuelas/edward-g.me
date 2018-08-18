b7fee985bd3b904baf9a32f54b7812e3263a68ca
Edward Ganuelas <epganuelas@gmail.com>
2018-08-18 18:05:04 -0400

    content update

--- src/copy/about.js
+++ src/copy/about.js
@@ -2,7 +2,7 @@ const about = [
     'Came to Canada in the year 2005',
     'First job was at McDonalds. Worked in the kitchen and eventually got promoted to Crew Trainer and Team Leader',
     'Went to George Brown College for the Computer Programmer Analyst program',
-    'Self diagnosed MBTI type is ISTP',
+    'Self diagnosed as a ISTP',
     'Thinks that Michael Jordan is the G.O.A.T.',
     'Loves Video Games',
     'Loves Pro Wrestling',
@@ -11,7 +11,7 @@ const about = [
     'Started programming as a kid because of video games',
     'Lifts Weights',
     'Swears that Keto is the best cutting diet',
-    'Favorite Colour is Blue',
+    'Favorite Colour is Blue, specifically #0066FF',
     'Usually wears clothes coloured blue, black, white, purple and even pink',
     'Thinks phones should always have a headphone jack',
     'Shoots film photography',
@@ -20,6 +20,9 @@ const about = [
     'Currently Single...', 
     'Has a replica wrestling title belt',
     'Had double jaw surgery (Ask me in person!)',
+    'Drives an old manual Civic',
+    'Doesn\'t consider himself a car guy even though he drives stick',
+    'Has more cameras than he has talent'
 ];
 
 export default about;
