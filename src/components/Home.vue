4a52e2ad9783a095fb67dd909d1067741bebecfb
Edward Ganuelas <epganuelas@gmail.com>
2018-08-18 17:17:39 -0400

    main content update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,11 +3,14 @@
     <div class="row">
       <div class="col-12 col-lg-7 main-content">
         <p>Hello, my name is Edward Paulo Ganuelas and this is my personal site.</p>
-        <p>There is nothing much here right now, but I plan to do a lot of rolling updates soon!</p>
+        <p>For those who are wondering, what is the purpose of this site? Well the purpose is that this site would focus more
+          on Edward the person more than Edward the developer. My hobbies, photographs, personal blog and small things about me would be found here.</p>
+        <p>If you are interested to know more about Edward the developer, checkout my other site <a href="https://eightrayedsun.com/" target="_blank" rel="noopener noreferrer">EightRayedSun</a> or my <a href="https://github.com/edward-ganuelas" target="_blank" rel="noopener noreferrer">Github</a> page.</p>
+        <!-- <p>There is nothing much here right now, but I plan to do a lot of rolling updates soon!</p>
         <p>You can follow the development of this site on it's Github <a href="https://github.com/edward-ganuelas/edward-g.me" target="_blank" rel="noopener noreferrer">repo.</a></p>
         <p>You can also checkout my other site, <a href="https://eightrayedsun.com/" target="_blank" rel="noopener noreferrer">EightRayedSun</a> or my development 
         <a href="https://blog.eightrayedsun.com/#/" target="_blank" rel="noopener noreferrer">blog.</a></p>
-        <p>Thank you, and check back soon!</p>
+        <p>Thank you, and check back soon!</p> -->
       </div>
     </div>
     <blog-widget />
@@ -71,6 +74,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 .main-content{
+  margin-bottom: 18px;
   @media(min-width: 1024px){
     padding-top: 120px;
   }
