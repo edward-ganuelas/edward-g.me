51286de40d2fb7d0f8dc2429d4bf75c2643574a1
Edward Ganuelas <epganuelas@gmail.com>
2019-12-07 19:18:14 -0500

    re-order nav links

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -8,14 +8,14 @@
             <span class='ico'><i class="fas fa-info-circle"></i></span>
             {{$t('nav.about')}}
         </router-link>
+        <router-link to="/blog">
+            <span class='ico'><i class="fas fa-file-alt"></i></span>
+                Blog
+        </router-link>
         <router-link to="/photography">
             <span class='ico'><i class="fas fa-camera-retro"></i></span>
             {{$t('nav.photography')}}
         </router-link>
-        <router-link to="/blog">
-            <span class='ico'><i class="fas fa-file-alt"></i></span>
-                Blog
-            </router-link>
         <a href="https://www.linkedin.com/in/epganuelas/" target="_blank" rel="noopener noreferrer" @click="tracking('linkedin')">
             <span class='ico'><i class='fab fa-linkedin'></i></span>
                 Linkedin
