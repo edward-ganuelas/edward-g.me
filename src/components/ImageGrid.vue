d59de42c5ac1ad437d29c5e6191d8cacb1be5bee
Edward Paulo Ganuelas <epganuelas@gmail.com>
2020-02-14 20:02:05 -0500

    redo sort

--- src/components/ImageGrid.vue
+++ src/components/ImageGrid.vue
@@ -2,13 +2,10 @@
     <div class="row">
         <div class="col-12 col-md-8 offset-md-2">  
             <h4>{{images.title}}</h4>
-            <label class="sort">{{$t('imageGrid.orderBy')}}
-                <select v-model="sort">
-                    <option value="old">{{$t('imageGrid.old')}}</option>
-                    <option value="new">{{$t('imageGrid.new')}}</option>
-                    <option value="random">{{$t('imageGrid.random')}}</option>
-                </select>
-            </label>
+            <p>{{$t('imageGrid.orderBy')}}</p>
+            <button class="btn btn-primary sort" @click="setSort(sortChoices.old)"><i class="fas fa-sort-numeric-up"></i> {{$t('imageGrid.old')}}</button>
+            <button class="btn btn-primary sort" @click="setSort(sortChoices.new)"><i class="fas fa-sort-numeric-up"></i> {{$t('imageGrid.new')}}</button>
+            <button class="btn btn-primary sort" @click="setSort(sortChoices.random)"><i class="fas fa-random"></i> {{$t('imageGrid.random')}}</button>
             <masonry :cols="2" :gutter="0">
                 <div v-for="image of grid" v-bind:key="image['index']">
                     <img v-img="{'title': image['title']}" :src="image['url']" :alt="image['title']" v-bind:class="image['orientation']" />
@@ -16,13 +13,16 @@
             </masonry>
         </div>
         <div class="col-12">
-            <button class="btn btn-primary" :disabled="this.gridSize == this.numberOfImages" @click="addGridSize">{{$t('imageGrid.seeMore')}} ({{remainingImages}})</button>
+            <button class="btn btn-primary loadMore" :disabled="this.gridSize == this.numberOfImages" @click="addGridSize">{{$t('imageGrid.seeMore')}} ({{remainingImages}})</button>
         </div>
     </div>
 </template>
 
 <script>
 import _ from 'lodash';
+
+const DEFAULT_SORT = 'old';
+
 export default {
     name: 'ImageGrid',
     props: {
@@ -34,7 +34,13 @@ export default {
     data() {
         return {
             gridSize: 8,
-            sort: 'old'
+            sort: DEFAULT_SORT,
+            isImagesShuffled: false,
+            sortChoices: Object.freeze({
+                old: 'old',
+                new: 'new',
+                random: 'random'
+            })
         };
     },
     methods: {
@@ -47,32 +53,32 @@ export default {
                 eventCategory: `Load More ${this.toggle}`,
                 eventAction: 'click',
             });
+        },
+        setSort(sort) {
+            this.sort = DEFAULT_SORT;
+            this.sort = sort;
         }
     },
     computed: {
         numberOfImages() {
             return _.size(this.images.images);
         },
-        shuffleImages() {
-            return _.shuffle(this.images.images);
-        },
         remainingImages() {
             const remainingImages = this.numberOfImages - this.gridSize;
             return remainingImages < 0 ? 0 : remainingImages;
         },
         sortedImages() {
-            if(this.sort !== 'random'){
-                let images = !_.isEmpty(this.images.images) ? this.images.images.slice() : [];
-                return images.sort((a,b) => {
-                    if(this.sort === 'old'){
-                        return a.id - b.id;
-                    }else if(this.sort === 'new'){
-                        return b.id - a.id;
-                    }
-                });
-            }else{
-                return this.shuffleImages;
+            if (this.sort === this.sortChoices.random) {
+                return !_.isEmpty(this.images.images) ? _.shuffle(this.images.images) : [];
             }
+            const images = !_.isEmpty(this.images.images) ? this.images.images.slice() : [];
+            return images.sort((a,b) => {
+                if (this.sort === 'old') {
+                    return a.id - b.id;
+                } else if(this.sort === 'new') {
+                    return b.id - a.id;
+                }
+            });
         },
         grid() {
             return _.slice(this.sortedImages, 0, this.gridSize);
@@ -97,7 +103,14 @@ img {
 h4 {
     margin: 20px 0;
 }
-button {
+label {
+    margin-right: 1rem;
+}
+.sort {
+    margin-right: 1rem;
+    margin-bottom: 1rem;
+}
+.loadMore {
     margin: 50px auto;
     display: block;
 }
