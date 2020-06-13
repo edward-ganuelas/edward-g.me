bbd7c6692c929525be5944298612d8f605d3cd6a
Edward Paulo Ganuelas <epganuelas@gmail.com>
2020-06-12 20:56:07 -0400

    styling update

--- src/App.vue
+++ src/App.vue
@@ -119,12 +119,14 @@ span.ico {
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
     transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
     &:hover {
-        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+        box-shadow: 0 14px 28px rgba(255,153,0,0.25), 0 10px 10px rgba(255,153,0,0.22);
     }
 }
 .card{
     padding: 25px 0;
     background-color: $cardBackgroundColor;
+    border-color: #0066FF;
+
     button {
         background-color: $buttonGrey;
         border-color: $buttonGrey;
