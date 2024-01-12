339ada45c04707684fad3f8405dc669b1bd983f3
Edward Paulo Ganuelas <epganuelas@gmail.com>
2024-01-12 17:37:22 -0500

    vue3 migration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite';
-// import vue from '@vitejs/plugin-vue';
-import { createVuePlugin as vue } from "vite-plugin-vue2";
+import vue from '@vitejs/plugin-vue';
+// import { createVuePlugin as vue } from "vite-plugin-vue2";
 import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue({
+        template: {
+            compilerOptions: {
+                compatConfig: {
+                    MODE: 3
+                }
+            }
+        }
+    })],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
+            vue: '@vue/compat'
         },
     },
 })
