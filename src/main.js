f8e300d06914d52d9867f979aba4d6c67c5e8614
edward-ganuelas <epganuelas@gmail.com>
2024-12-31 12:21:18 -0500

    remove vue-head

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import { createApp } from 'vue'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueHead from 'vue-head'
-import VueAnalytics from 'vue-analytics';
 import store from './vuex/store';
 import i18n from '@/copy/site-copy';
 import Ripple from 'vue-ripple-directive';
@@ -14,17 +12,16 @@ Ripple.color = 'rgba(0, 102, 255, 0.9)';
 
 const app = createApp(App)
 
-app.use(VueHead);
 app.use(store);
 app.use(router);
 app.use(i18n)
-app.use(VueAnalytics, {
-    id: 'UA-112626956-1',
-    router,
-    debug: {
-        sendHitTask: process.env.NODE_ENV === 'production'
-    }
-})
+// app.use(VueAnalytics, {
+//     id: 'UA-112626956-1',
+//     router,
+//     debug: {
+//         sendHitTask: process.env.NODE_ENV === 'production'
+//     }
+// })
 app.directive('ripple', Ripple);
 
 app.mount('#app');
