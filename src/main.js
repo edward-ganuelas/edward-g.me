9f55df78df8f7425ce2617f4c0c241ccd51498c0
Edward Paulo Ganuelas <epganuelas@gmail.com>
2024-12-31 13:06:23 -0500

    remove ripple

--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ import App from './App.vue'
 import router from './router'
 import store from './vuex/store';
 import i18n from '@/copy/site-copy';
-import Ripple from 'vue-ripple-directive';
 import VueGtagPlugin from 'vue-gtag';
 
-Ripple.color = 'rgba(0, 102, 255, 0.9)';
 
 const app = createApp(App)
 
