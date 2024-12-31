cea80150d5e17b175e5c005df0619e6f9e918ff4
Edward Paulo Ganuelas <epganuelas@gmail.com>
2024-12-31 15:22:47 -0500

    update vue-i18n, fix html warnings

--- src/views/About/Bio.vue
+++ src/views/About/Bio.vue
@@ -7,7 +7,27 @@
         </div>
         <div class="row">
             <div class="col-12 col-md-8 offset-md-2 card shadow">
-                <div class="card-body" v-html="$t('about.bio.content')"></div>
+                <!-- <div class="card-body" v-html="$t('about.bio.content')"></div> -->
+                <div class="card-body">
+                    <i18n-t keypath="about.bio.paragraph1" tag="p"></i18n-t>
+                    <i18n-t keypath="about.bio.paragraph2" tag="p"></i18n-t>
+                    <i18n-t keypath="about.bio.paragraph3" tag="p">
+                        <template v-slot:twitter>
+                            <a href="https://twitter.com/edward_pau" target="_blank"  rel="noopener noreferrer"><i class="fab fa-twitter"></i>@edward_pau</a>
+                        </template>
+                        <template v-slot:instagram>
+                            <a href="https://www.instagram.com/edward.g.me/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i> instagram</a>
+                        </template>
+                        <template v-slot:linkedIn>
+                            <a href="https://www.linkedin.com/in/epganuelas/" target="_blank" rel="noopener noreferrer"><span class='ico'><i class='fab fa-linkedin'></i></span>Linkedin</a>
+                        </template>
+                    </i18n-t>
+                    <i18n-t keypath="about.bio.paragraph4" tag="p">
+                        <template v-slot:eightRayedSun>
+                            <a href="https://eightrayedsun.com/" target="_blank" rel="noopener noreferrer">EightRayedSun</a>
+                        </template>
+                    </i18n-t>
+                </div>
             </div>
         </div>
     </div>
