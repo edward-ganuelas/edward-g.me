fc0818ed8be2e6d0a6ad2bb1fec87e703c6b380c
Edward Paulo Ganuelas <epganuelas@gmail.com>
2024-12-31 12:42:57 -0500

    remove unused import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import { createApp } from 'vue'
-import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './vuex/store';
